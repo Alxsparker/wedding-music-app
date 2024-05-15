0277675eee088b82b561fd9396986ea38d9c305e
Alxsparker <alexandre.chalon@gmail.com>
2024-05-15 21:32:25 +0200

    Fix unused variable errors in functions/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+const functions = require('firebase-functions');
+const logger = require('firebase-functions/logger');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Exemple d'utilisation de onRequest
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+  logger.info("HelloWorld function called");  // Exemple d'utilisation de logger
+});
+/* eslint-disable no-unused-vars */
+const functions = require('firebase-functions');
+const onRequest = functions.https.onRequest;
+const logger = require('firebase-functions/logger');
+/* eslint-enable no-unused-vars */
+
+// Exemple d'utilisation de onRequest
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
